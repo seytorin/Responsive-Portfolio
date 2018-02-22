16cad3ea2fde62eae2a7751bf8971dbab0a1ee97
Your Name <seytorin@yahoo.com>
2018-02-22 13:29:33 -0600

    Added gif to portfolio hangman link

--- server.js
+++ server.js
@@ -26,8 +26,6 @@ app.use(express.static(path.join(__dirname, './app/public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
-
-
 app.use(bodyParser.text());
 // ================================================================================
 // ROUTER
